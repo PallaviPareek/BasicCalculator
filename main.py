37d58bf7ddb803909763499496c305aea46f572a
PallaviPareek <pallavipareek311@gmail.com>
2020-06-05 19:09:41 +0530

    Magical Calculator

---
+++ main.py
@@ -0,0 +1,26 @@
+import re
+print("Our Magical Calculator")
+print("Type 'quit' to exit\n ")
+previous = 0
+run = True
+def performMath():
+    global run
+    global previous
+    equation =""
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+    if equation == 'quit':
+        print("Good Bye")
+        run = False
+    else:
+        equation = re.sub('[A-Za-z,.()" "]','',equation)
+        if previous==0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous)+equation)
+
+
+while run:
+    performMath()
